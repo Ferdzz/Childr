de1f4fd4df6a6d988dcfe4d80a3948b532948061
Dizzy <didiercamous@gmail.com>
2016-05-10 23:57:13 -0400

    Switched the radio buttons to image buttons

--- app/src/main/java/com/childr/childr/MainActivity.java
+++ app/src/main/java/com/childr/childr/MainActivity.java
@@ -1,10 +1,12 @@
 package com.childr.childr;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,33 +17,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final RadioButton rdGirlNames = (RadioButton) findViewById(R.id.girl_names);
-        final RadioButton rdBoyNames = (RadioButton) findViewById(R.id.boy_names);
-        final RadioButton rdBothNames = (RadioButton) findViewById(R.id.both_names);
+        final ImageButton btnGirl = (ImageButton) findViewById(R.id.female_button);
+        final ImageButton btnBoy = (ImageButton) findViewById(R.id.male_button);
+        final ImageButton btnIntersex = (ImageButton) findViewById(R.id.intersex_button);
 
-        Button btnConfirm = (Button) findViewById(R.id.btnConfirm);
-
-        assert btnConfirm != null;
-        btnConfirm.setOnClickListener(new View.OnClickListener() {
+        assert btnGirl != null;
+        assert btnBoy != null;
+        assert btnIntersex != null;
+        btnGirl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String gender = null;
-
-                if (rdGirlNames.isChecked()) {
-                    gender = "girl";
-                } else if (rdBoyNames.isChecked()) {
-                    gender = "boy";
-                } else if (rdBothNames.isChecked()) {
-                    gender = "both";
-                }
-
-                if (gender != null) {
-                    Intent intent = new Intent(getApplicationContext(), SwipeScreenActivity.class);
-                    intent.putExtra("gender", gender);
-
-                    startActivity(intent);
-                }
+                sendIntent("girl");
             }
         });
+        btnBoy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendIntent("boy");
+            }
+        });
+        btnIntersex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendIntent("both");
+            }
+        });
+    }
+
+    public void sendIntent(String pExtra) {
+        Intent intent = new Intent(getApplicationContext(), SwipeScreenActivity.class);
+        intent.putExtra("gender", pExtra);
+
+        startActivity(intent);
     }
 }
